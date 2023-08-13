06270b11d284b4c043942fca9e9f627217cc856c
juandavidvargas19 <114268705+juandavidvargas19@users.noreply.github.com>
2023-08-13 18:57:04 -0500

    Delete update_docs_requirements.sh

--- update_docs_requirements.sh
+++
@@ -1,7 +0,0 @@
-REQUIREMENTS_PATH=docs/requirements.txt
-
-echo "upgrade pip"
-poetry run pip install --upgrade pip
-echo "poetry install"
-poetry install
-poetry export -f requirements.txt --with dev --without-hashes -o $REQUIREMENTS_PATH
