fd1afdc90c7480038dd91cc0f9dabe22ff7bbb76
juandavidvargas19 <114268705+juandavidvargas19@users.noreply.github.com>
2023-08-12 20:41:58 -0500

    Update update_docs_requirements.sh

--- update_docs_requirements.sh
+++ update_docs_requirements.sh
@@ -5,4 +5,3 @@ poetry run pip install --upgrade pip
 echo "poetry install"
 poetry install
 poetry export -f requirements.txt --with dev --without-hashes -o $REQUIREMENTS_PATH
-echo "hypyp==$(poetry run python -c 'import hypyp; print(hypyp.__version__)')" >> $REQUIREMENTS_PATH
